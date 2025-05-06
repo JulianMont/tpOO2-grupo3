457d994946914c1b6ca683c3d0b5285f9d801adf
Diego Ibañez <diego.hernan.ibanez@gmail.com>
2025-05-06 19:40:34 -0300

    Base del proyecto

---
+++ src/datos/Turno.java
@@ -0,0 +1,15 @@
+package datos;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Turno {
+	private int idTurno;
+	private LocalDate fecha;
+	private LocalDateTime horaTurno;
+	private Cliente cliente;
+	private Empleado empleado;
+	private Servicio servicio;
+	private EstadoTurno estado;
+		
+}
